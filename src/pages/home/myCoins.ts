3529d1df156e8dca2a02284dcdb8c63c155f98fd
ok <oleg.koleskin@gmail.com>
2018-01-11 22:31:35 +0200

    WIP

---
+++ src/pages/home/myCoins.ts
@@ -0,0 +1,97 @@
+export function returnMyCoins() {
+  return [
+      {
+          symbol: 'BTC',
+          amount: 0.00649733
+      },
+      {
+          symbol: 'ETH',
+          amount: 1
+      },
+      {
+          symbol: 'LTC',
+          amount: 4.78643971
+      },
+      {
+          symbol: 'ETH',
+          amount: 0.00144237
+      },
+      {
+          symbol: 'BCC',
+          amount: 0.00428754
+      },
+      {
+          symbol: 'EOS',
+          amount: 1
+      },
+      {
+          symbol: 'OMG',
+          amount: 2.58
+      },
+      {
+          symbol: 'BTC',
+          amount: 0.43196472
+      },
+      {
+          symbol: 'BTC',
+          amount: 0.00002654
+      },
+      {
+          symbol: 'ETH',
+          amount: 2.3120795
+      },
+      {
+          symbol: 'ETC',
+          amount: 0.99378848
+      },
+      {
+          symbol: 'XMR',
+          amount: 1.000001
+      },
+      {
+          symbol: 'DASH',
+          amount: 0.6991182
+      },
+      {
+          symbol: 'XRP',
+          amount: 266.82035329
+      },
+      {
+          symbol: 'MIOTA',
+          amount: 50
+      },
+      {
+          symbol: 'BCH',
+          amount: 0.15725223
+      },
+      {
+          symbol: 'NEO',
+          amount: 0.92439729
+      },
+      {
+          symbol: 'XEM',
+          amount: 269.70166551
+      },
+      {
+          symbol: 'XLM',
+          amount: 400
+      },
+      {
+          symbol: 'DOGE',
+          amount: 3000
+      },
+      {
+          symbol: 'ADA',
+          amount: 50
+      },
+      {
+          symbol: 'XVG',
+          amount: 100
+      },
+      {
+          symbol: 'BTC',
+          amount: 0.00001612
+      },
+  ];
+}
+
