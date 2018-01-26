f473568a64bbe36d421b856e77592ddd5bd52002
ok <oleg.koleskin@gmail.com>
2018-01-26 23:18:28 +0200

    WIP

--- src/shared/coinmarketcap.service.ts
+++ src/shared/coinmarketcap.service.ts
@@ -5,14 +5,20 @@ import { Http } from '@angular/http';
 @Injectable()
 export class CoinMarketCapApi {
 
-  private url : string = 'https://api.coinmarketcap.com/v1/ticker/?limit=100&convert=EUR';
+  private url: string = 'https://api.coinmarketcap.com/v1/ticker/?limit=100&convert=EUR';
 
   constructor(private http: Http) { }
 
   getCurrences() {
-    return new Promise (resolve => {
+    return new Promise(resolve => {
       this.http.get(this.url)
-        .subscribe (res => resolve(res.json()) )
+        .subscribe(
+        res => resolve(res.json()),
+        err => {
+          console.log(err);
+          alert('error calling coinmarketcap');
+        }
+        )
     })
   }
 }
